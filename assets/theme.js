c505889cc8ac9e2b33544cde45bd6b1f2ea2f44f
fabsharp <Omahabitch14>
2020-02-26 10:36:58 +0100

    add highlightjs

--- assets/theme.js
+++ assets/theme.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($) {
     // Right menu on pages
     var $list = $('#sidebar-right');
+    var buffer = "";
     $("#main section").find("h1, h2, h3, h4").each(function(title, element){
         var $element = $(element);
-        $list.append('<li><a href="#'+ $element.attr('id') +'" class="'+ $element.prop('tagName') +'">'+ $element.html() +'</a></li>');
-
+        buffer += '<li><a href="#'+ $element.attr('id') +'" class="'+ $element.prop('tagName') +'">'+ $element.html() +'</a></li>';
     });
+    $list.prepend(buffer);
     UIkit.scrollspy($list[0]);
-
     // Babylon links
     $('a[href^="https://doc.babylonjs.com"]').each(function(index, element) {
         $(element).attr('target', '_blank');
